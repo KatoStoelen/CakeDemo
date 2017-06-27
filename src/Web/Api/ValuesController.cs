e7765eedd92593058e863654bd0b93e6b9b8bc2e
Kato Stølen <kato.stolen@itverket.no>
2017-06-27 14:57:47 +0200

    Removed slash in route

--- src/Web/Api/ValuesController.cs
+++ src/Web/Api/ValuesController.cs
@@ -3,7 +3,7 @@ using System.Web.Http;
 
 namespace Web.Api
 {
-    [RoutePrefix("/api/values")]
+    [RoutePrefix("api/values")]
     public class ValuesController : ApiController
     {
         [HttpGet]
